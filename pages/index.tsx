6c3337b5789fcc3068a097c923873918f488f3fe
rzkzx <romanzidanramadhan@gmail.com>
2021-09-04 23:12:42 +0800

    404 Custom page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout>
       <h1>Home Page</h1>
     </Layout>
-  )
+  );
 }
