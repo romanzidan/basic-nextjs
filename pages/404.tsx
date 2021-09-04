aff284151f67633df486981862072a01e2f8967f
rzkzx <romanzidanramadhan@gmail.com>
2021-09-04 23:17:18 +0800

    Auto redirection Next JS

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,13 @@
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
+
 export default function Custom404() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 2000);
+  }, []);
   return (
     <div>
       <h1 className="title-not-found">Ooops...</h1>
