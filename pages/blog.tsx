e5cae9dc98f2dedf5190690c101b810cfbb679af
rzkzx <romanzidanramadhan@gmail.com>
2021-09-04 19:18:25 +0800

    Simple Routing

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,7 @@
 export default function blog() {
-    return (
-        <div>
-            <p>Blog Page</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>Blog Page</p>
+    </div>
+  );
 }
