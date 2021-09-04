7100ab43fc7f9cb30e244b337fa053b8b2db553b
rzkzx <romanzidanramadhan@gmail.com>
2021-09-04 18:44:44 +0800

    Setup Eslint Next JS

---
+++ pages/blog.tsx
@@ -0,0 +1,7 @@
+export default function blog() {
+    return (
+        <div>
+            <p>Blog Page</p>
+        </div>
+    )
+}
---
+++ pages/users/detail.tsx
@@ -0,0 +1,7 @@
+export default function UserDetail() {
+    return (
+        <div>
+            <h2>User Detail Page</h2>
+        </div>
+    )
+}
