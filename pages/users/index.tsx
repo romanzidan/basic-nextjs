2c23745619725c04e46bab02fb8685aa65bb55fd
rzkzx <romanzidanramadhan@gmail.com>
2021-09-05 12:00:09 +0800

    Data Fetching getStaticProps

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,30 @@
 import Layout from '../../components/Layout';
 
-export default function Users() {
+interface UsersProps {
+  dataUsers: Array<any>;
+}
+
+export default function Users(props: UsersProps) {
+  const { dataUsers } = props;
+  console.log(dataUsers);
   return (
     <Layout pageTitle="User Page">
-      <h1>User Page</h1>
+      {dataUsers.map((user) => (
+        <>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </>
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
 }
