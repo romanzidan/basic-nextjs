edcdbcd33c16957e1047746d3aebf6789b313aa9
rzkzx <romanzidanramadhan@gmail.com>
2021-09-05 08:49:38 +0800

    Optimalisasi Image Next JS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout pageTitle="Home Page">
+      <Image src="/profile.jpg" width={200} height={200} alt="profile" />
       <h1>Home Page</h1>
     </Layout>
   );
